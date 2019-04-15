6f1cd6e26d8f432abbae1c32511730bebeaaa230
Jared Crain <jared.crain@wsu.edu>
2019-04-15 14:37:05 -0700

    Display usernames for reviews, books, and authors

--- app/models/author.js
+++ app/models/author.js
@@ -3,6 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   first: DS.attr('string'),
   last: DS.attr('string'),
-  
-  books: DS.hasMany('book')
+  username: DS.attr('string'),
+
+  books: DS.hasMany('book'),
 });
--- app/models/book.js
+++ app/models/book.js
@@ -1,10 +1,11 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-    title: DS.attr('string'),
-    isbn: DS.attr('string'),
-    publishDate: DS.attr(),
-    
-    author: DS.belongsTo('author'),
-    reviews: DS.hasMany('review')
+  title: DS.attr('string'),
+  isbn: DS.attr('string'),
+  publishDate: DS.attr(),
+  username: DS.attr('string'),
+
+  author: DS.belongsTo('author'),
+  reviews: DS.hasMany('review'),
 });
